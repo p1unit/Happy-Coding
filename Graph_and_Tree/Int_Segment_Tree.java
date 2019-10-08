e44a9f3e906a996197abcbc82f857e151c8eef40
Puneet Kumar <puneet16638@gmail.com>
2019-10-08 18:09:39 +0530

    segment tree added

---
+++ Graph_and_Tree/Int_Segment_Tree.java
@@ -0,0 +1,55 @@
+package Graph_and_Tree;
+class Int_Segment_Tree{
+
+    int[] tree;
+    int MAX=2000001;
+
+    public Int_Segment_Tree() {
+        this.tree=new int[MAX];
+    }
+
+    private int action(int parent1, int parent2) {
+        // ToDo :- add method function here
+        return 0;
+    }
+
+    private int outOfBound() {
+        // ToDo - add out of bound value here
+        return 0;
+    }
+
+    public void build(int[] arr,int idx,int start,int end){
+        if(start==end){
+            this.tree[idx]=arr[start];
+            return;
+        }
+        int mid=(start+end)/2;
+        build(arr,2*idx+1,start,mid);
+        build(arr,2*idx+2,mid+1,end);
+        this.tree[idx]=action(this.tree[2*idx+1],this.tree[2*idx+2]);
+    }
+
+    public int query(int idx,int l,int r,int start,int end){
+        if(start>r || end<l)
+            return outOfBound();
+        if(start>=l && end<=r)
+            return this.tree[idx];
+        int mid=(start+end)/2;
+        return action(query(2*idx+1,l,r,start,mid),query(2*idx+2,l,r,mid+1,end));
+    }
+
+    public void update(int[] arr,int idx,int n,int start,int end){
+        if(start==end){
+            this.tree[idx]=arr[start];
+            return;
+        }
+        int mid=(start+end)/2;
+        if(n<=mid)
+            update(arr,2*idx+1,n,start,mid);
+        else
+            update(arr,2*idx+2,n,mid+1,end);
+
+        this.tree[idx]=action(this.tree[2*idx+1],this.tree[idx+2]);
+    }
+
+}
